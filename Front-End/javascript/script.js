3d4f248a7ef2704fbf5bd51daaab192ec8aabd75
Yuri Lins <yurilins1332@gmail.com>
2025-11-23 16:12:07 -0300

    btn cadastrar

--- Front-End/javascript/script.js
+++ Front-End/javascript/script.js
@@ -1,18 +1,51 @@
-const abrirModalBtn = document.getElementById('abrirModalBtn');
-const fecharModalBtn = document.getElementById('fecharModalBtn');
-const modalOverlay = document.getElementById('modalOverlay');
+const modalLogin = document.getElementById('modalLogin');
+const abrirLoginBtn = document.getElementById('abrirLoginBtn'); 
+const fecharLoginBtn = document.getElementById('fecharLoginBtn'); 
 
-abrirModalBtn.addEventListener('click', () => {
-    modalOverlay.classList.add('ativo');
+const modalCadastro = document.getElementById('modalCadastro');
+const abrirCadastroNavBtn = document.getElementById('abrirCadastroNavBtn'); 
+const fecharCadastroBtn = document.getElementById('fecharCadastroBtn'); 
+
+const irParaCadastroBtn = document.getElementById('irParaCadastroBtn');
+const irParaLoginBtn = document.getElementById('irParaLoginBtn');
+
+abrirLoginBtn.addEventListener('click', () => {
+    modalLogin.classList.add('ativo');
+    modalCadastro.classList.remove('ativo'); // Garante que o outro feche
 });
 
-fecharModalBtn.addEventListener('click', () => {
-    modalOverlay.classList.remove('ativo');
+abrirCadastroNavBtn.addEventListener('click', () => {
+    modalCadastro.classList.add('ativo');
+    modalLogin.classList.remove('ativo');
 });
 
 
-modalOverlay.addEventListener('click', (evento) => {
-    if (evento.target === modalOverlay) {
-        modalOverlay.classList.remove('ativo');
+fecharLoginBtn.addEventListener('click', () => {
+    modalLogin.classList.remove('ativo');
+});
+
+fecharCadastroBtn.addEventListener('click', () => {
+    modalCadastro.classList.remove('ativo');
+});
+
+irParaCadastroBtn.addEventListener('click', () => {
+    modalLogin.classList.remove('ativo');
+    modalCadastro.classList.add('ativo');
+});
+
+irParaLoginBtn.addEventListener('click', () => {
+    modalCadastro.classList.remove('ativo');
+    modalLogin.classList.add('ativo');
+});
+
+modalLogin.addEventListener('click', (evento) => {
+    if (evento.target === modalLogin) {
+        modalLogin.classList.remove('ativo');
+    }
+});
+
+modalCadastro.addEventListener('click', (evento) => {
+    if (evento.target === modalCadastro) {
+        modalCadastro.classList.remove('ativo');
     }
 });
